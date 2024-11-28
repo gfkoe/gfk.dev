6301070f82c39f1fb3dfcc5ec4b7539f930223b1
Gabe Koeb <gfkoeb@ncsu.edu>
2024-11-28 18:29:23 -0500

    Add some info about myself and init shadcn

---
+++ src/components/custom/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="text-2xl">
+        Hey &#128075; ! My name is <strong>Gabe</strong>, and I am a new
+        graduate in <code>Computer Science</code> from{" "}
+        <strong>NC State University</strong>.
+      </h1>
+    </header>
+  );
+}
