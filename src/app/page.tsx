2b35c9505800a93fab0c09038c15bbd3589a5e1f
Gabe Koeb <gfkoeb@ncsu.edu>
2024-11-28 21:56:19 -0500

    Add info card and fix styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,38 @@ import {
 } from "../components/ui/card";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-24">
+    <main className="flex min-h-screen flex-col md:p-24 p-2">
       <Header />
       <br />
-      <Card className="w-1/2">
-        <CardHeader>
-          <CardTitle>About Me</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>
-            I tend to specialize in front-end web development, but I have some
-            experience with back-end development and api design as well. I am
-            currently looking for a full-time software engineerint position to
-            start in the new year.
-          </p>
-        </CardContent>
-      </Card>
+      <div className="flex flex-col md:flex-row justify-between gap-4 gap-4 h-full">
+        <Card className="md:max-w-[49.5%] max-h-[49.5%]">
+          <CardHeader>
+            <CardTitle>About Me</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>
+              I tend to specialize in frontend web development, but I have some
+              experience with backend and api design as well. I am currently
+              looking for a full-time software engineerint position to start in
+              the new year.
+            </p>
+          </CardContent>
+        </Card>
+
+        <Card className="md:max-w-[49.5%] max-h-[49.5%]">
+          <CardHeader>
+            <CardTitle>About Me</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>
+              I tend to specialize in front-end web development, but I have some
+              experience with back-end development and api design as well. I am
+              currently looking for a full-time software engineerint position to
+              start in the new year.
+            </p>
+          </CardContent>
+        </Card>
+      </div>
     </main>
   );
 }
--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,10 +1,9 @@
 export default function Header() {
   return (
     <header>
-      <h1 className="text-2xl">
-        Hey &#128075; ! My name is <strong>Gabe</strong>, and I am a new
-        graduate in <code>Computer Science</code> from{" "}
-        <strong>NC State University</strong>.
+      <h1 className="text-4xl">
+        Hey &#128075; ! My name is <strong>Gabe</strong>, and I am a new grad in{" "}
+        <code>Computer Science</code> from <strong>NC State University</strong>.
       </h1>
     </header>
   );
